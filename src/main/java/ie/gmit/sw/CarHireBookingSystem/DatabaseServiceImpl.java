1eef74a3e4fe62980ea7419e09b4e9091fa97fe2
nakster <g00332403@gmit.ie>
2018-10-29 00:48:51 +0000

    added classes

---
+++ src/main/java/ie/gmit/sw/CarHireBookingSystem/DatabaseServiceImpl.java
@@ -0,0 +1,42 @@
+package ie.gmit.sw.CarHireBookingSystem;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class DatabaseServiceImpl extends UnicastRemoteObject implements DatabaseService{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected DatabaseServiceImpl() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String Create(String s) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Read() throws RemoteException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String Update(String s) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String Delete(String s) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
