a9bdcd983e1c76394f4f39fa29ae8b77cd9a4bd5
nakster <g00332403@gmit.ie>
2018-11-21 19:35:42 +0000

    project rmi + jersey shell done

---
+++ CarHireRMI/src/ie/gmit/sw/CarHireRest/CarHireServiceImpl.java
@@ -0,0 +1,44 @@
+package ie.gmit.sw.CarHireRest;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class CarHireServiceImpl extends UnicastRemoteObject implements CarHireService{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected CarHireServiceImpl() throws RemoteException {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String Create(String s) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void Read() throws RemoteException {
+		System.out.println("Working");
+		
+	}
+
+	@Override
+	public String Update(String s) throws RemoteException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String Delete(String s) throws RemoteException {
+		// TODO Auto-generated method stub
+		System.out.println("Working");
+		
+		return null;
+	}
+
+}
