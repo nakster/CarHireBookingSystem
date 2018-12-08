d40981d7f4d1d81605179a15657b8d1f98325d0e
nakster <g00332403@gmit.ie>
2018-12-08 19:22:29 +0000

    adding comments for red()

--- DS/RMI/src/ie/gmit/sw/rmi/DatabaseServiceImpl.java
+++ DS/RMI/src/ie/gmit/sw/rmi/DatabaseServiceImpl.java
@@ -32,10 +32,12 @@ public class DatabaseServiceImpl extends UnicastRemoteObject implements Database
 	public List<Order> read() throws RemoteException, SQLException {
 		System.out.println("In read");
 
+		//create connection
 		stmt = conn.createStatement();
-
+		//create a list to store the mysql objects
 		orderList = new ArrayList<Order>();
 	
+		//prepare statement
 		String strSelect = "select orderid , Date, \r\n" + 
 				"c.CustID,c.FirstName,c.LastName,c.Mobile,c.Country,c.Email,\r\n" + 
 				"ca.CarID, ca.CarColour, ca.CarBrand, ca.CarModel, ca.PurchaseDate\r\n" + 
@@ -45,12 +47,16 @@ public class DatabaseServiceImpl extends UnicastRemoteObject implements Database
 				"INNER JOIN CAR as ca\r\n" + 
 				"ON Orders.CarID=ca.CarID ORDER BY orderid";
 
+		//execute the query
 		ResultSet rset = stmt.executeQuery(strSelect);
 
+		//this add the objects the the order class 
 		while (rset.next()) { 
+			//get the order class obkects 
 			int OrderID = rset.getInt("OrderID");
 			Date Date = rset.getDate("Date");
 			
+			//get the customer class objects 
 			int cust = rset.getInt("c.CustID");
 			String name = rset.getString("c.FirstName");
 			String lname = rset.getString("c.LastName");
@@ -58,20 +64,23 @@ public class DatabaseServiceImpl extends UnicastRemoteObject implements Database
 			String country = rset.getString("c.Country");
 			String email = rset.getString("c.Email");
 
-			
+			//get the car class objects 
 			int car = rset.getInt("ca.CarID");
 			String carColour = rset.getString("ca.CarColour");
 			String carBrand = rset.getString("ca.CarBrand");
 			String carModel = rset.getString("ca.CarModel");
 			Date purchaseDate = rset.getDate("ca.PurchaseDate");
 			
+			//this adds all the customer objects to the class in the constructor 
 			Customer c = new Customer(cust,name,lname,mobile,country,email);
-			
+			//this adds all the car objects to the class in the constructor 
 			Car ca = new Car(car, carColour,carBrand,carModel,purchaseDate);
-		
+			//this adds all the order objects to the class in the constructor 
 			Order s = new Order(OrderID, Date,ca, c);
+			// this here adds the order object to the arraylist which is going to be sent to the jersey project
 			orderList.add(s);
 		}
+		// returns the arraylist of order class
 		return orderList;
 	}
 
